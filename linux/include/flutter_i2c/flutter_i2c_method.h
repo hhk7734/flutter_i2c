d40f533cdc21d22579967c9c26f46ef4019bc00f
Hyeonki Hong <hhk7734@gmail.com>
2021-04-10 03:23:14 +0900

    linux: include: method: update docs

--- linux/include/flutter_i2c/flutter_i2c_method.h
+++ linux/include/flutter_i2c/flutter_i2c_method.h
@@ -15,9 +15,35 @@ const char kReceiveMethod[]    = "receive";
 const char kTransceiveMethod[] = "transceive";
 
 namespace flutter_i2c {
+/**
+ * @param args int fd
+ */
 FlMethodResponse *init(FlValue *args);
+
+/**
+ * @param args int fd
+ */
 FlMethodResponse *dispose(FlValue *args);
+
+/**
+ * @param args 0: int fd
+ * @param args 1: int slaveAddress
+ * @param args 2: Uint8List byteData
+ */
 FlMethodResponse *transmit(FlValue *args);
+
+/**
+ * @param args 0: int fd
+ * @param args 1: int slaveAddress
+ * @param args 2: int size
+ */
 FlMethodResponse *receive(FlValue *args);
+
+/**
+ * @param args 0: int fd
+ * @param args 1: int slaveAddress
+ * @param args 2: Uint8List txByteData
+ * @param args 3: int rxSize
+ */
 FlMethodResponse *transceive(FlValue *args);
 }    // namespace flutter_i2c
